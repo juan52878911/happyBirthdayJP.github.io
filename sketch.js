b81fcceb177581180ad106535a82a87d197283fb
JuanPBedoya <juanbedoya843@gmail.com>
2023-04-21 14:09:40 -0500

    Fix confetti

--- sketch.js
+++ sketch.js
@@ -94,4 +94,20 @@ function windowResized() {
   _text.fill(167, 230, 217 );
   _text.noStroke();
   _text.text(text, width * 0.5, height * 0.5);
+
+  for (let i = 0; i < confetti.length / 2; i++) {
+    confetti[i].confettiDisplay();
+
+    if (confetti[i].y > height) {
+      confetti[i] = new Confetti(random(0, width*2), random(-height, 0), random(-1, 1));
+    }
+  }
+
+  for (let i = int(confetti.length / 2); i < confetti.length; i++) {
+    confetti[i].confettiDisplay();
+
+    if (confetti[i].y > height) {
+      confetti[i] = new Confetti(random(0, width*2), random(-height, 0), random(-1, 1));
+    }
+  }
 }
